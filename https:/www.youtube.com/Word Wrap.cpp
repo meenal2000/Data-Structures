588dcadc1c498e05d1e5675f7ed8aa164221e3a0
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-08-14 18:59:57 +0530

    Update Word Wrap.cpp

--- https:/www.youtube.com/Word Wrap.cpp
+++ https:/www.youtube.com/Word Wrap.cpp
@@ -29,3 +29,33 @@ int solveWordWrap(vector<int>nums, int k)
         }
         return dp[0];
     } 
+// through recursion
+int solve(vector<int> &nums, int k, vector<int> &dp, int n, int i,int len)
+    {
+        if(i < 0)
+            return dp[i+1];
+        int cost;
+        for(int j=i; j<n; ++j)
+        {
+            len += nums[j] + 1;
+            if(len > k)
+                break;
+            if(j==n-1)
+               cost = 0;
+            else
+                cost = (k-len)*(k-len) + dp[j+1];
+            if(dp[i] > cost)
+            {
+                dp[i] = cost;
+            }
+        }
+        //cout << dp[i] << endl;
+        return solve(nums,k,dp,n,i-1,-1);
+    }
+    int solveWordWrap(vector<int>nums, int k) 
+    { 
+        int n = nums.size();
+        vector<int> dp(n,INT_MAX);
+        dp[n-1] = 0;
+        return solve(nums, k, dp, n, n-2 ,-1);
+    } 
