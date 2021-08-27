578c9789c50b385a0c7cd997f2f6bd37d9c2748b
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-08-27 09:28:17 +0530

    Create ConvertBSTToMinHeap.cpp

---
+++ Heaps section/ConvertBSTToMinHeap.cpp
@@ -0,0 +1,16 @@
+// just do the inorder traversal , store in array
+// do pre-order trav and paste the nodes value from inorder trav
+// Observation based ques
+// it will generate min-heap satisfying BST property
+                 4
+               /   \
+              2     6
+            /  \   /  \
+           1   3  5    7  
+                   
+             Answer      
+                1
+              /   \
+             2     5
+           /  \   /  \
+          3   4  6    7 
