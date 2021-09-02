d11736fe44d52c7232643e3f4af3a2bf7dbe5a90
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-09-02 18:25:59 +0530

    Update and rename temp.cpp to Implement two stacks in an array.cpp

---
+++ Stacks-and-queues/Implement two stacks in an array.cpp
@@ -0,0 +1,32 @@
+void twoStacks :: push1(int x)
+{
+    this->top1++;
+    this->arr[top1] = x;
+}
+   
+//Function to push an integer into the stack2.
+void twoStacks ::push2(int x)
+{
+    this->top2--;
+    this->arr[top2] = x;
+}
+   
+//Function to remove an element from top of the stack1.
+int twoStacks ::pop1()
+{
+    if(top1 == -1)
+        return -1;
+    int pop = arr[top1];
+    top1--;
+    return pop;
+}
+
+//Function to remove an element from top of the stack2.
+int twoStacks :: pop2()
+{
+    if(top2 == size)
+        return -1;
+    int pop = arr[top2];
+    top2++;
+    return pop;
+}
