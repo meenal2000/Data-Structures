eae5663fb8d861996b9821e01b4e4d5572d4bb68
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-09-03 13:00:54 +0530

    Create Min Stack.cpp

---
+++ Stacks-and-queues/Min Stack.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<pair<int,int> > s;
+    MinStack() {
+    }
+    void push(int x) {
+        if(s.empty())
+        {
+            s.push({x,x});
+        }
+        else
+        {
+            s.push({x,min(x,s.top().second)});
+        }
+    }
+    
+    void pop() {
+          s.pop();
+    }
+    
+    int top() {
+        return s.top().first;
+    }
+    
+    int getMin() {
+        return s.top().second;
+    }
+    
+};
