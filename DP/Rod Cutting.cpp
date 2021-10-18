ffa32b1d1b0b870032eee5b53aab278289b585fc
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-10-18 21:47:04 +0530

    Create Rod Cutting.cpp

---
+++ DP/Rod Cutting.cpp
@@ -0,0 +1,17 @@
+int cutRod(int price[], int n) {
+        int dp[n+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i = 1; i<=n; ++i)
+        {
+            for(int j = 1; j<=n; ++j)
+            {
+                if(i <= j) // i is current length & array index both
+                {
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-i]+price[i-1]);
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][n];
+    }
